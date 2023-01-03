a1154e81483a9ac99b53fd5572b4af57fa4aec46
Theo Fornetti <theoforentti2001@gmail.com>
2023-01-03 17:02:41 -0300

    3/1

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 module.exports = {
-  
   // Conexión
   username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "contra",
-  database: process.env.DB_DATABASE || "sequelize",
-  host: process.env.DB_HOST || "localhost",
-  dialect: process.env.DB_DIALECT || "mysql",
+  password: process.env.DB_PASSWORD|| "contra",
+  database: process.env.DB_DATABASE||"sequelize",
+  host: process.env.DB_HOST||"localhost",
+  // port: 6517 || 3036,
+  dialect:  process.env.DB_DIALECT||"mysql",
   
   // Configurar Seeds
   seederStorage: "sequelize",
